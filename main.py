8cc677b5afee7b3b5ab93924f495a3022412574a
waniwanipanikku <166777220+waniwanipanikku@users.noreply.github.com>
2024-06-20 12:00:43 +0900

    index

--- main.py
+++ main.py
@@ -48,6 +48,6 @@ def index():
     """
     return HTMLResponse(content=html_content, status_code=200)
 
-    @app.post("/present")
-async def new_naming(present):
-    return {"response": f"サーバです。メリークリスマス！ {present}ありがとう。お返しはキャンディーです。"}
+#     @app.post("/present")
+# async def new_naming(present):
+#     return {"response": f"サーバです。メリークリスマス！ {present}ありがとう。お返しはキャンディーです。"}
