fa214889e9d7ee6f09b2b61519bc1b16377c52d2
waniwanipanikku <166777220+waniwanipanikku@users.noreply.github.com>
2024-06-20 11:41:48 +0900

    present

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "takoyak🐙"}
+    return {"message": "takoyak🐙ika"}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
